9f24bdfa33415580574c44dd865673b071b2bbca
Syed Riyyan Hassan <riyyanhassan789@gmail.com>
2026-05-21 12:13:15 +0500

    added input fields, however no logic is yet implemented using js script

---
+++ script.js
@@ -0,0 +1,37 @@
+let total_tip = 0               // total tip = bill * tip percentage
+let total_bill = 0              // total bill = bill + tip
+let share_per_person = 0        // share per person = total bill / people
+
+let billInputs = {
+    people: document.getElementById('people'), 
+    bill: document.getElementById('bill'), 
+    custom: document.getElementById('custom'),
+}
+
+function totalTip(billInputs)
+{
+    // a function that calculates the total tip
+
+    return total_tip
+}
+
+function totalBill(billInputs)
+{
+    //  a funtion that calculates the total bill
+
+    return total_bill
+}
+
+function shareOfPeople(billInputs)
+{
+    // a function that calculates the share of every person
+
+    return share_per_person
+}
+
+// a function that would display outputs
+
+function display()
+{
+    // display calculations
+}
